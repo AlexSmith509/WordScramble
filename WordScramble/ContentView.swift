9bd2d85decbc833d795a8f518618612d8a983b21
Alex Smith <alexwsmith@me.com>
2023-04-30 22:19:48 -0700

    Added 3 functions to validate words submitted

--- WordScramble/ContentView.swift
+++ WordScramble/ContentView.swift
@@ -11,6 +11,10 @@ struct ContentView: View {
     @State private var usedWords = [String]()
     @State private var rootWord = ""
     @State private var newWord = ""
+    
+    @State private var errorTitle = ""
+    @State private var errorMessage = ""
+    @State private var showingError = false 
     var body: some View {
         NavigationView {
             List {
@@ -24,6 +28,7 @@ struct ContentView: View {
                         ForEach(usedWords, id: \.self) { word in
                             Image(systemName: "\(word.count).circle")
                             Text(word)
+                           
                         }
                     }
                 }
@@ -40,7 +45,7 @@ struct ContentView: View {
         
         // extra validation to come
         
-        withAnimation {
+        withAnimation() {
             usedWords.insert(answer, at: 0)
         }
         newWord = ""
@@ -56,6 +61,31 @@ struct ContentView: View {
         }
         fatalError("Could not load start.txt from bundle")
     }
+    func isOriginal (word: String) -> Bool {
+        !usedWords.contains(word)
+    }
+    func isPossible (word: String) -> Bool {
+        var tempWord = rootWord
+        
+        for letter in word {
+            if let pos = tempWord.firstIndex(of: letter) {
+                tempWord.remove(at: pos)
+            } else {
+                return false
+            }
+        }
+        
+        return true
+    }
+    
+    func isReal(word: String) -> Bool {
+        let checker = UITextChecker()
+        let range = NSRange(location: 0, length: word.utf16.count)
+        let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
+        
+        return misspelledRange.location == NSNotFound
+    }
+    
 }
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
